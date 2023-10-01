05dd70394cc940973e6b3c4ba9cae5b414bf260b
koson <567256+koson@users.noreply.github.com>
2023-10-02 02:27:41 +0700

    Update main.c

--- main/main.c
+++ main/main.c
@@ -15,15 +15,17 @@
 int state = 0;
 xQueueHandle interputQueue;
 
+// interrupt service handler
 static void IRAM_ATTR gpio_interrupt_handler(void *args)
 {
     int pinNumber = (int)args;
     xQueueSendFromISR(interputQueue, &pinNumber, NULL);
 }
 
+// LED control task, received button prerssed from ISR
 void LED_Control_Task(void *params)
 {
-    int pinNumber, count = 0;
+    int pinNumber = 0;
     while (true)
     {
         if (xQueueReceive(interputQueue, &pinNumber, portMAX_DELAY))
@@ -36,19 +38,22 @@ void LED_Control_Task(void *params)
 
 void app_main()
 {
+	// LED config as I/O port
     gpio_pad_select_gpio(LED_PIN);
     gpio_set_direction(LED_PIN, GPIO_MODE_INPUT_OUTPUT);
 
+    // config Input No 21 for external interrupt input
     gpio_pad_select_gpio(INPUT_PIN);
     gpio_set_direction(INPUT_PIN, GPIO_MODE_INPUT);
     gpio_pulldown_en(INPUT_PIN);
     gpio_pullup_dis(INPUT_PIN);
     gpio_set_intr_type(INPUT_PIN, GPIO_INTR_POSEDGE);
 
-
+    // FreeRTOS tas and queue
     interputQueue = xQueueCreate(10, sizeof(int));
     xTaskCreate(LED_Control_Task, "LED_Control_Task", 2048, NULL, 1, NULL);
 
+    // install isr service and isr handler
     gpio_install_isr_service(0);
     gpio_isr_handler_add(INPUT_PIN, gpio_interrupt_handler, (void *)INPUT_PIN);
 }
